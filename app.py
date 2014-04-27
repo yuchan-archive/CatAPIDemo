84d63b4dcee62fb8f86c16199dc6c84b4f92e56a
Yusuke Ohashi <yusuke@junkpiano.me>
2014-04-28 00:13:56 +0900

    cat images

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
 import os
 from flask import Flask
 
+import requests
+import urllib
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'hello world!'
+    urlEndPoint = 'http://thecatapi.com'
+    imageGetPath = '/api/images/get'
+    values = {'format':'html',
+            'results_per_page':10}
+    r = requests.get(urlEndPoint + imageGetPath + "?" +  urllib.urlencode(values))
+    return r.text
+
+if __name__ == "__main__":
+    app.run(port=8000)
 
